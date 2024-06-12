3a05da5d05daf7d853f04083a244b28bf02e6c54
= <=>
2024-06-12 19:55:01 +0900

    feat: Create setting current position and add function
    - 현재 위치로 이동 버튼 생성 및 위치 고정
    - 다른 지역으로 이동했다가 버튼 클릭 시 현재 위치로 되돌아오는 기능 추가

--- src/components/Maps/index.tsx
+++ src/components/Maps/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useEffect, useState } from "react";
 import GoogleMapReact from 'google-map-react';
+import Center from "./center";
 
 
 const GoogleMaps = ():JSX.Element => {
@@ -31,17 +32,46 @@ const GoogleMaps = ():JSX.Element => {
         })
     }
 
+    const handleApiLoaded = (map: any, maps: any) => {
+        // Use the map and maps objects here if needed
+    };
+
+    /** 지도가 이동할 때마다 center 값을 update하는 함수 */
+    const handleMapChange = ({ center }: any) => {
+        setCurrentPosition(center)
+    }
+
+    /** 유저 현재 위치를 중심으로 지도를 이동시키는 함수 */
+    const moveCenterCurrentPosition = () => {
+        if ( navigator.geolocation ) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const newCenter = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                setCurrentPosition(newCenter)
+            })
+        } else {
+            alert('Geolocation is not supported by this browser')
+        }
+    }
+
     return (
-        <div style = {{ width: '100%', height: '100vh' }}>
+        <div style = {{ width: '100%', height: '100vh', position: 'relative'}}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAP_API || ''}}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
                 center={currentPosition}
                 zoom={16}
+                onChange={handleMapChange}
+                yesIWantToUseGoogleMapApiInternals
+                onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
             >
-
             </GoogleMapReact>
+
+            <Center moveMap={moveCenterCurrentPosition} />
         </div>
     )
 }
